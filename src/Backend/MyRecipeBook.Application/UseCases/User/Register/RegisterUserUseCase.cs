e5eb6fd35e2d30823a5c3f1361140b78865db340
gustavocaruso1 <gustavocaruso82@gmail.com>
2025-09-18 07:15:23 -0300

    Implementação de mensagens de erro personalizadas.

---
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserUseCase.cs
@@ -0,0 +1,41 @@
+﻿using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Communication.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRecipeBook.Application.UseCases.User.Register
+{
+    public class RegisterUserUseCase
+    {
+        public ResponseRegisteredUserJson Execute(RequestRegisterUserJson request)
+        {
+            Validate (request);
+            //validar a request
+
+            //mapear a request em uma entidade
+
+            //criptografia da senha
+
+            //salvar no banco de dados
+            return new ResponseRegisteredUserJson
+            {
+                Name = request.Name
+            };
+        }
+        private void Validate(RequestRegisterUserJson request)
+        {
+            var validator = new RegisterUserValidator();
+            var result = validator.Validate(request);
+
+            if (result.IsValid == false)
+            {
+                var errorMessages = result.Errors.Select(e => e.ErrorMessage);
+                throw new Exception();
+                    
+            }
+        }
+    }
+}
---
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRecipeBook.Application.UseCases.User.Register
+{
+    public class RegisterUserValidator : AbstractValidator<RequestRegisterUserJson>
+    {
+        public RegisterUserValidator() 
+        {
+            RuleFor(user => user.Name).NotEmpty().WithMessage(ResourceMessagesException.NAME_EMPTY);
+            RuleFor(user => user.Email).NotEmpty();
+            RuleFor(user => user.Email).EmailAddress();
+            RuleFor(user => user.Password.Length).GreaterThanOrEqualTo(8);
+        }
+
+    }
+}
