6df6e6b8631cac4347d6b007491a2b05980bec06
gustavocaruso1 <gustavocaruso82@gmail.com>
2026-01-15 18:16:43 -0300

    Criando Schema via API

---
+++ src/Backend/MyRecipeBook.Infrastructure/Migrations/DatabaseMigration.cs
@@ -0,0 +1,39 @@
+﻿using Dapper;
+using MyRecipeBook.Domain.Enums;
+using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRecipeBook.Infrastructure.Migrations
+{
+    public static class DatabaseMigration
+    {
+        public static void Migrate(DatabaseType databaseType, string connectionString)
+        {
+            EnsureDatabaseCreated_MySql(connectionString); 
+        }
+        private static void EnsureDatabaseCreated_MySql(string connectionString)
+        {
+            var connectionStringBuilder = new MySqlConnectionStringBuilder(connectionString);
+
+            var databaseName = connectionStringBuilder.Database;
+
+            connectionStringBuilder.Remove("Database");
+
+            using var dbConnection = new MySqlConnection(connectionStringBuilder.ConnectionString);
+
+            var parameters = new DynamicParameters();
+
+            parameters.Add("name", databaseName);
+
+            var records = dbConnection.Query("SELECT * FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = @name", parameters);
+            if (records.Any() == false)
+            
+                dbConnection.Execute($"CREATE DATABASE {databaseName}");
+            
+        }
+    }
+}
